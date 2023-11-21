a229345621f77106e252285080ce1dbbb426476a
Behzod Musurmonqulov <behzodmusurmonqulov@gmail.com>
2023-11-21 19:59:17 +0500

    Initial commit

---
+++ src/pages/Presentation/sections/Counters.js
@@ -0,0 +1,59 @@
+/*
+=========================================================
+* Material Kit 2 React - v2.1.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/material-kit-react
+* Copyright 2023 Creative Tim (https://www.creative-tim.com)
+
+Coded by www.creative-tim.com
+
+ =========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+*/
+
+// @mui material components
+import Container from "@mui/material/Container";
+import Grid from "@mui/material/Grid";
+import Divider from "@mui/material/Divider";
+import MKBox from "components/MKBox";
+import DefaultCounterCard from "examples/Cards/CounterCards/DefaultCounterCard";
+
+function Counters() {
+  return (
+    <MKBox component="section" py={3}>
+      <Container>
+        <Grid container item xs={12} lg={9} sx={{ mx: "auto" }}>
+          <Grid item xs={12} md={4}>
+            <DefaultCounterCard
+              count={100}
+              suffix="+"
+              title="Staff"
+              description="Passionate and dedicated educators empowering student success"
+            />
+          </Grid>
+          <Grid item xs={12} md={4} display="flex">
+            <Divider orientation="vertical" sx={{ display: { xs: "none", md: "block" }, mx: 0 }} />
+            <DefaultCounterCard
+              count={7}
+              suffix=".0"
+              title="IELTS score"
+              description="Average score "
+            />
+            <Divider orientation="vertical" sx={{ display: { xs: "none", md: "block" }, ml: 0 }} />
+          </Grid>
+          <Grid item xs={12} md={4}>
+            <DefaultCounterCard
+              count={4}
+              title="Pages"
+              description="Save 3-4 weeks of work when you use our pre-made pages for your website"
+            />
+          </Grid>
+        </Grid>
+      </Container>
+    </MKBox>
+  );
+}
+
+export default Counters;
