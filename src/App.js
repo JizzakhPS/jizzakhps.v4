7fb1f6ecd00f09458940b90c28e9ed3c6f9f49e4
behzodmusurmonqulov <behzodmusurmonqulov@gmail.com>
2024-11-26 18:21:42 +0000

    Git

--- src/App.js
+++ src/App.js
@@ -44,7 +44,7 @@ export default function App() {
       <Routes>
         {getRoutes(routes)}
         <Route path="/" element={<Presentation />} />
-        <Route path="/behzod" element={<StudentProfile student={student} />} />
+        <Route path="/students/behzod" element={<StudentProfile student={student} />} />
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </ThemeProvider>
